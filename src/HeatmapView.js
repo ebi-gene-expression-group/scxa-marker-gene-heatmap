ce1806f1d9d9563cd5997a6fd880e70c6622e4bb
lingyun1010 <zhaolingyun1010@gmail.com>
2019-06-25 11:03:28 +0100

    Correct yAxis based on marker gene and cluster

--- src/HeatmapView.js
+++ src/HeatmapView.js
@@ -144,7 +144,8 @@ class HeatmapView extends React.Component {
                 data={filteredData}
                 isDataFiltered={selectedClusterId && selectedClusterId.value !== `all` || false}
                 xAxisCategories={allClusterIds}
-                yAxisCategories={ _.uniq(data.map(x => x.geneName))}
+                yAxisCategories={ _.uniq(data.map(x => `${x.geneName}#${x.clusterIdWhereMarker}`))
+                  .map(gene => gene.substring(0, gene.indexOf(`#`))) }
                 chartHeight={defaultHeatmapHeight}
                 hasDynamicHeight={_.uniq(filteredData.map(x => x.geneName)).length > 5 ? hasDynamicHeight : false} // don't want dynamic height if there is little or no data
                 heatmapRowHeight={heatmapRowHeight}
